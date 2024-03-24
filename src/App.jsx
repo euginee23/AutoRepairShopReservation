5499513a740e03d8f9c98a560b381dad94185b02
euginee23 <eugineimba123@gmail.com>
2024-03-24 13:52:57 +0800

    removed unused forms

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Registration from './pages/Registration';
 import Login from './pages/Login';
 import MainPage from './pages/MainPage';
-import EmailVerificationProcess from './pages/EmailVerificationProcess';
-import EmailVerificationSuccess from './pages/EmailVerificationSuccess';
 function App() {
   return (
     <Router>
@@ -12,8 +10,6 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Registration />} />
-        <Route path="/email-verification-success" element={<EmailVerificationSuccess />} />
-        <Route path="/email-verification-process" element={<EmailVerificationProcess />} />
         <Route path="/main" element={<MainPage />} />
       </Routes>
     </Router>
